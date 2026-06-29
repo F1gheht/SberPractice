ac260325a1b8396841b9db7c0ca05926dceaaedc
F1gheht <72334533+F1gheht@users.noreply.github.com>
2026-06-29 16:36:39 +0300

    Add files via upload

---
+++ 1p/Task2.java
@@ -0,0 +1,7 @@
+public class Task2 {
+    public static void main(String[] args) {
+        int a = 21;
+        int b = 8;
+        System.out.println(a + " / " + b + " = " + (a / b) + " и " + (a % b) + " в остатке");
+    }
+}
