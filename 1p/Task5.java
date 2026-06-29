beeaff101c8247cada2eee37080f144c75db2318
F1gheht <72334533+F1gheht@users.noreply.github.com>
2026-06-29 17:01:34 +0300

    Add files via upload

---
+++ 1p/Task5.java
@@ -0,0 +1,10 @@
+public class Task5 {
+    public static void main(String[] args) {
+
+        double earthWeight = 100;
+
+        double moonWeight = earthWeight * 0.17;
+
+        System.out.println(moonWeight);
+    }
+}
