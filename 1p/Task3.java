a5d0d2bbd883a661bec498d6459f7e026c3b940b
F1gheht <72334533+F1gheht@users.noreply.github.com>
2026-06-29 16:44:46 +0300

    Add files via upload

---
+++ 1p/Task3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Task3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine().trim();
+
+        String reversed = new StringBuilder(input).reverse().toString();
+
+        System.out.println(reversed);цц
+
+        if (input.equals(reversed)) {
+            System.out.println("Число является палиндромом.");
+        } else {
+            System.out.println("Число не является палиндромом.");
+        }
+    }
+}
