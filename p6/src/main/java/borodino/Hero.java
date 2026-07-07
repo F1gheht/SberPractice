63104d2ddd4f7973279e10b5a1a9be25b96467a0
F1gheht <72334533+F1gheht@users.noreply.github.com>
2026-07-07 12:26:27 +0300

    Add files via upload

---
+++ p6/src/main/java/borodino/Hero.java
@@ -0,0 +1,15 @@
+package borodino;
+
+import java.io.Serializable;
+
+public class Hero implements Serializable {
+    String name;
+    int lvl;
+    Weapon weapon;
+
+    public Hero(String name, int lvl, Weapon weapon) {
+        this.name = name;
+        this.lvl = lvl;
+        this.weapon = weapon;
+    }
+}
---
+++ p6/src/main/java/borodino/Weapon.java
@@ -0,0 +1,13 @@
+package borodino;
+
+import java.io.Serializable;
+
+public class Weapon implements Serializable {
+    String name;
+    int damage;
+
+    public Weapon(String name, int damage) {
+        this.name = name;
+        this.damage = damage;
+    }
+}
